6b992b76a818fd87cfa28dee131c001427642984
Antonio Raimundo <antoniorrm@Mac-mini-Dj.local>
2020-03-16 12:26:02 -0300

    lib for reading the barcode was added and tested

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import ScannerBarCode from "./pages/ScannerBarCode";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <ScannerBarCode />
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
@@ -12,8 +14,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff"
+  }
 });
